2f4b180d67271c8221e612c5bfef5ae4d7c287e5
Mujtaba Omer <mujtaba.Omer30@outlook.com>
2022-08-16 21:12:40 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+togglerBtn = document.querySelector('.nav-toggler')
+toggleMenu = document.querySelector('.toggler-menu')
+
+togglerBtn.addEventListener('click', () => {
+    toggleMenu.classList.toggle('hide')
+})
